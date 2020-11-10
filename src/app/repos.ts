b6f5b4b5969a41f958514b9209351a4b6561ff1c
keza1brenda <keza1brenda@gmail.com>
2020-11-11 01:08:27 +0200

    create search component

--- src/app/repos.ts
+++ src/app/repos.ts
@@ -1,4 +1,8 @@
 export class Repos {
-    constructor(public name: string, public html_url: string, public description: string, public created_at: Date) {
+    constructor(
+        public name: string, 
+        public html_url: string, 
+        public description: string, 
+        public created_at: Date) {
     }
 }
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { Repos } from '../repos';
+import { Search } from '../search';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
